287b27ed5d3c7ebec3942fc61b6832d9d0ab11d6
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-01-25 22:00:38 +0100

    :tada: Initial commit

---
+++ android/app/src/main/kotlin/com/dbenfouzari/wish_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dbenfouzari.wish_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Se connecter"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Toto'),
+        ),
+      ),
+    );
+  }
+}
